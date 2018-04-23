1212fe6b93ffd1e5fd84deb458bcf5adf774f002
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MessageBoxServiceExample/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxServiceExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ribbonControl1.SelectedPage = ribbonControl1.Pages["Test"];
+            #region #replaceservice
+            MyMessageBoxService svc = new MyMessageBoxService(spreadsheetControl1, spreadsheetControl1.LookAndFeel);
+            spreadsheetControl1.ReplaceService<DevExpress.XtraSpreadsheet.Services.IMessageBoxService>(svc);
+            #endregion #replaceservice
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            SendKeys.Send("=foo;{ENTER}");
+        }
+    }
+}
---
+++ VB/MessageBoxServiceExample/MyMessageBoxService.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+#Region "#usings"
+Imports System.Windows.Forms
+Imports DevExpress.LookAndFeel
+Imports DevExpress.XtraEditors
+Imports DevExpress.XtraSpreadsheet.Services
+#End Region ' #usings
+
+
+Namespace MessageBoxServiceExample
+	#Region "#service"
+	Friend Class MyMessageBoxService
+		Implements DevExpress.XtraSpreadsheet.Services.IMessageBoxService
+		Private ReadOnly control As Control
+		Private ReadOnly lookAndFeel As UserLookAndFeel
+
+		Public Sub New(ByVal control As Control, ByVal lookAndFeel As UserLookAndFeel)
+			Me.control = control
+			Me.lookAndFeel = lookAndFeel
+		End Sub
+
+		Public Function ShowWarningMessage(ByVal message As String) As DialogResult Implements IMessageBoxService.ShowWarningMessage
+			Dim mymessage As String = "Es ist ein Fehler aufgetreten. Bitte überprüfen Sie die Eingabe." & Constants.vbLf + Constants.vbLf & message
+			Return XtraMessageBox.Show(lookAndFeel, control, mymessage, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning)
+		End Function
+		Public Function ShowOkCancelMessage(ByVal message As String) As Boolean Implements IMessageBoxService.ShowOkCancelMessage
+			Dim mymessage As String = "Möchten Sie diesen Vorgang wirklich ausführen?" & Constants.vbLf + Constants.vbLf & message
+			Return DialogResult.OK = XtraMessageBox.Show(lookAndFeel, control, mymessage, Application.ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Warning)
+		End Function
+	End Class
+	#End Region ' #service
+End Namespace
