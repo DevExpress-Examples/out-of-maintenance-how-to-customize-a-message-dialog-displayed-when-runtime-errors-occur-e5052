a2312516b84235639cfac3bfcea44874abd87948
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MessageBoxServiceExample/MyMessageBoxService.cs
@@ -0,0 +1,41 @@
+﻿#region #usings;
+using System.Windows.Forms;
+using DevExpress.LookAndFeel;
+using DevExpress.XtraEditors;
+#endregion #usings
+
+
+namespace MessageBoxServiceExample
+{
+    #region #service
+    class MyMessageBoxService : DevExpress.XtraSpreadsheet.Services.IMessageBoxService
+    {
+        readonly Control control;
+        readonly UserLookAndFeel lookAndFeel;
+
+        public MyMessageBoxService(Control control, UserLookAndFeel lookAndFeel)
+        {
+            this.control = control;
+            this.lookAndFeel = lookAndFeel;
+        }
+
+        public DialogResult ShowWarningMessage(string message)
+        {
+            string mymessage = "Es ist ein Fehler aufgetreten. Bitte überprüfen Sie die Eingabe.\n\n" + message;
+            return XtraMessageBox.Show(lookAndFeel, control, mymessage, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        public bool ShowOkCancelMessage(string message)
+        {
+            string mymessage = "Möchten Sie diesen Vorgang wirklich ausführen?\n\n" + message;
+            return DialogResult.OK == XtraMessageBox.Show(lookAndFeel, control, mymessage, Application.ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        public bool ShowYesNoMessage(string message)
+        {
+            string mymessage = "Entscheidung treffen. Ja oder Nein?\n\n" + message;
+            return DialogResult.Yes == XtraMessageBox.Show(lookAndFeel, control, mymessage, Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+
+    }
+    #endregion #service
+}
---
+++ VB/MessageBoxServiceExample/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Namespace MessageBoxServiceExample
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+			ribbonControl1.SelectedPage = ribbonControl1.Pages("Test")
+'			#Region "#replaceservice"
+			Dim svc As New MyMessageBoxService(spreadsheetControl1, spreadsheetControl1.LookAndFeel)
+			spreadsheetControl1.ReplaceService(Of DevExpress.XtraSpreadsheet.Services.IMessageBoxService)(svc)
+'			#End Region ' #replaceservice
+		End Sub
+
+		Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles barButtonItem1.ItemClick
+			SendKeys.Send("=foo;{ENTER}")
+		End Sub
+	End Class
+End Namespace
